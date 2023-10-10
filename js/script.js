688644c15c516dc092da4ecb589a5111450b5733
AugustoM94 <email@github.com>
2023-10-10 17:33:15 +0200

    added function on js

--- js/script.js
+++ js/script.js
@@ -1,10 +1,32 @@
-let kilometersNumber;
-let passengerAge;
-const button = document.querySelector('btn--success');
-const resetButton = document.querySelector('button.reset-button');
+let km;
+let age;
+let Username;
+const button = document.querySelector('btn-success');
+const resetButton = document.querySelector('btn-danger');
 let prezzo_scontato = 0;
 let Prezzo_finale;
 const discountUnder18 = 20/100;
 const discountOver65 = 40/100;
 let Prezzo_biglietto = (km * 0.21);
 
+button.addEventListener('click',
+    function() {
+        Username = parseInt(document.getElementById('name').value);
+        km = parseInt(document.getElementById('km').value);
+        age = parseInt(document.getElementById('age').value);
+        if (age < 18) {
+            prezzo_scontato = (Prezzo_biglietto * discountUnder18);
+            Prezzo_finale = (Prezzo_biglietto - prezzo_scontato);
+        } else if (age > 65) {
+            prezzo_scontato = (Prezzo_biglietto * discountOver65);
+            Prezzo_finale = (Prezzo_biglietto - prezzo_scontato);
+        } else   {        
+        const Username = document.getElementsByClassName('.outputName');
+        const Prezzo_finale =document.getElementsByClassName('.outputCosto')
+        }
+
+        if(isNaN(km) || isNaN(age)) {
+            location.reload();
+        }})
+
+       
