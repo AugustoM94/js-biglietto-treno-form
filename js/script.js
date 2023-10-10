639a640d097933dde9a1fb5211bc9b20a198e79f
AugustoM94 <email@github.com>
2023-10-10 17:03:25 +0200

    add var on js

---
+++ js/script.js
@@ -0,0 +1,10 @@
+let kilometersNumber;
+let passengerAge;
+const button = document.querySelector('btn--success');
+const resetButton = document.querySelector('button.reset-button');
+let prezzo_scontato = 0;
+let Prezzo_finale;
+const discountUnder18 = 20/100;
+const discountOver65 = 40/100;
+let Prezzo_biglietto = (km * 0.21);
+
